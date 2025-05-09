894417da528cb87196e30e74c6525df92dbe39e3
matias <matiferreyraacade@gmail.com>
2025-05-08 23:24:53 -0300

    Hasta ajercicio s

---
+++ 05.1 Listas/Ejercicio_2.py
@@ -0,0 +1,7 @@
+# 2) Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo. 
+# ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
+
+lista = ["Celeste", 5, "Racing", 1990, "Videojuegos"]
+penultimo = lista[-2]
+
+print(penultimo)
